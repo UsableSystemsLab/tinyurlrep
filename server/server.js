88abfd25e1e2c1ef7289ffab955f81208a35c1d0
Khalid Alharbi <kalharbi@users.noreply.github.com>
2023-01-12 22:42:12 +0300

    fix add & get endpoints

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const { Pool } = require("pg");
 const express = require("express");
+const bodyParser = require('body-parser');
 var cors = require("cors");
 const nanoid = require("nanoid");
 require("dotenv").config();
@@ -16,32 +17,33 @@ const pool = new Pool({
 });
 
 const app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json({ type: 'application/*+json' }));
 app.use(cors());
 
-const client = pool.connect();
-// sanity check!
-// client.query("Select * from tinyurl", (err, res) => {
-//   if (!err) console.log(res.rows);
-//   else console.log(err.message);
-// });
+
 
 // HTTP POST: store the short code for the long url
-// HTTP POST http://host:port/save body -> https://github.com => gdfte545 => store in postgresdb => respond with short code
-app.post("/save", (req, res) => {
-  var short = nanoid.nanoid();
-  var long = req.body;
-  var vals = [long, short];
-  client.query(
+// HTTP POST http://host:port/add body -> https://github.com => gdfte545 => store in postgresdb => respond with short code
+app.post("/add", (req, res) => {
+  const short = nanoid.nanoid();
+  const long_url = req.body.url;
+  pool.query(
     "INSERT INTO public.tinyurl(long_url, short_code) VALUES($1, $2)",
-    vals
+    [long_url, short]
   );
-  // console.log("I got this ", req.body);
-  res.send(`www.amm48.me/${nanoid.nanoid()}`);
+  res.send({code: `${short}`});
 });
+
 // HTTP GET: get the long url by the short code
 // HTTP GET http://host:port/get?code=gdfte545 => postgres => return to longurl => client will HTTP redirect
-app.get("/get", (req, res) => {
-  res.send("Hello World");
+app.get("/get", async (req, res) => {
+  const code  = req.query.code;
+  const { rows } = await pool.query('SELECT long_url FROM public.tinyurl WHERE short_code = $1', [code])
+  res.send(rows[0]);
 });
 
 // HTTP GET requests
@@ -52,6 +54,3 @@ app.get("/health", (req, res) => {
 app.listen(PORT, HOST, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
 });
-
-client.release();
-pool.end();
